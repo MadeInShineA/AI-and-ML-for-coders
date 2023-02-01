9739967a556073525472617b4969c9fe8c291b12
MadeInShineA <108631757+MadeInShineA@users.noreply.github.com>
2023-02-01 18:01:45 +0100

    added files

---
+++ basic clothes.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+
+class Mycallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        if logs.get("accuracy") > 0.95:
+            print("\nReached 95% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+data = tf.keras.datasets.fashion_mnist
+
+callback = Mycallback()
+(training_images, training_labels),  (test_images, test_labels) = data.load_data()
+
+training_images = training_images/255
+test_images = test_images/255
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(training_images, training_labels, epochs=50, callbacks=[callback])
+
+model.evaluate(test_images, test_labels)
